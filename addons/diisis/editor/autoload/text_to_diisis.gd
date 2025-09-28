c14e1ceb8cfbe4b0685fdba4ab6dc270018dea8f
Fio <leukert.fio@gmail.com>
2025-09-28 20:13:28 +0200

    noise improv + importing bugfix

--- addons/diisis/editor/autoload/text_to_diisis.gd
+++ addons/diisis/editor/autoload/text_to_diisis.gd
@@ -142,9 +142,12 @@ func override_existing_data(imported_lines:Array, payload:={}, actor_ingestion_o
 	var new_page_data := {}
 	
 	var handled_line_ids := []
-	
 	var imported_lines_per_page := []
 	var working_page := []
+	
+	if not (imported_lines.front() as String).begins_with("PAGE"):
+		imported_lines.insert(0, "PAGE")
+	
 	var page_content_line : String = imported_lines.pop_front()
 	working_page.append(page_content_line)
 	while not imported_lines.is_empty():
@@ -186,6 +189,7 @@ func override_existing_data(imported_lines:Array, payload:={}, actor_ingestion_o
 		
 		var line_data_on_page := []
 		for lines : Array in lines_for_line_content:
+			#print(lines)
 			var line_data := {}
 			var line_declaration = lines.pop_front()
 			
@@ -266,7 +270,7 @@ func override_existing_data(imported_lines:Array, payload:={}, actor_ingestion_o
 	var path := Pages.editor.get_save_path()
 	var data = Pages.serialize()
 	var data_to_save := {}
-	data_to_save["editor"] = Pages.editor.serialize()
+	#data_to_save["editor"] = Pages.editor.serialize()
 	data_to_save["original_path"] = Pages.editor.get_save_path()
 	data_to_save["pages"] = Pages.serialize()
 	data_to_save["pages"]["page_data"] = new_page_data
