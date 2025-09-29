184488f0f2112ca6198580fbca5343c6d5890768
Fio <leukert.fio@gmail.com>
2025-09-29 21:25:56 +0200

    date activities

--- game/stages/rolling_credits.gd
+++ game/stages/rolling_credits.gd
@@ -2,17 +2,23 @@ extends Control
 
 
 func _ready() -> void:
-	visible = false
+	hide()
 
 
 
 func start():
-	visible = true
+	show()
 	
-	var label : Label = find_child("Label")
+	var label : RichTextLabel = find_child("Label")
 	
-	for text in ["DIISIS template demo", "created by Snek Remilia Ketter"]:
-		label.text = text
+	for text in [
+		"Angelarium",
+		"Created by Stella and Snek",
+		"Created for NOISE JAM"]:
+		label.text = "[shake rate=10.0 level=5 connected=1]%s[/shake]" % text
 		await get_tree().create_timer(2).timeout
 	
-	Parser.function_acceded()
+	await get_tree().create_timer(1).timeout
+	label.hide()
+	await get_tree().create_timer(3).timeout
+	get_tree().quit()
